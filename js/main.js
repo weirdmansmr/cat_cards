76732c950673ec2e1c73e0e68a033564994c7c79
weirdmansmr <maga.saykhanov95@gmail.com>
2020-08-29 10:33:50 +0300

    Улучшен код js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-function card1() {
-  const card_main = document.getElementsByClassName("container__1")[0];
-  const card = document.getElementsByClassName("card_1")[0];
-  const card_top_text = document.getElementsByClassName("card_text1")[0];
-  const card_bottom = document.getElementsByClassName("text")[0];
-  const card_corner = document.getElementsByClassName("corner")[0];
-  const card_circle = document.getElementsByClassName("card_text5")[0];
-  const button = document.getElementsByClassName("buy")[0];
+function card1_2(num) {
+  let card_main;
+  let card;
+  const card_top_text = document.getElementsByClassName("card_text1")[num];
+  const card_bottom = document.getElementsByClassName("text")[num];
+  const card_corner = document.getElementsByClassName("corner")[num];
+  const card_circle = document.getElementsByClassName("card_text5")[num];
+  const button = document.getElementsByClassName("buy")[num];
+
+  if (num == 0) {
+    card_main = document.getElementsByClassName("container__1")[0];
+    card = document.getElementsByClassName("card_1")[0];
+  } else if (num == 1) {
+    card_main = document.getElementsByClassName("container__2")[0];
+    card = document.getElementsByClassName("card_2")[0];
+  }
 
   function aftermod() {
     card_top_text.style.color = "#D91667";
@@ -36,10 +44,17 @@ function card1() {
     card_corner.style.background =
       "linear-gradient(-45deg,white, white 28.5px, #D91667 0,#D91667 32px, transparent 0)";
     card_circle.style.backgroundColor = "#D91667";
-    card_bottom.innerHTML = card_bottom.innerHTML.replace(
-      "Чего сидишь? Порадуй кота, ",
-      "Печень утки разварная с артишоками"
-    );
+    if (num == 0) {
+      card_bottom.innerHTML = card_bottom.innerHTML.replace(
+        "Чего сидишь? Порадуй кота, ",
+        "Печень утки разварная с артишоками"
+      );
+    } else if (num == 1) {
+      card_bottom.innerHTML = card_bottom.innerHTML.replace(
+        "Чего сидишь? Порадуй кота, ",
+        "Головы щучьи с чесноком да свежайшая сёмгушка."
+      );
+    }
     button.style.display = "none";
     card.addEventListener("mouseleave", aftermod);
   } else {
@@ -49,10 +64,18 @@ function card1() {
     card_corner.style.background =
       "linear-gradient(-45deg,white, white 28.5px, #1698D9 0,#1698D9 32px, transparent 0)";
     card_circle.style.backgroundColor = "#1698D9";
-    card_bottom.innerHTML = card_bottom.innerHTML.replace(
-      "Печень утки разварная с артишоками",
-      "Чего сидишь? Порадуй кота, "
-    );
+    if (num == 0) {
+      card_bottom.innerHTML = card_bottom.innerHTML.replace(
+        "Печень утки разварная с артишоками",
+        "Чего сидишь? Порадуй кота, "
+      );
+    } else if (num == 1) {
+      card_bottom.innerHTML = card_bottom.innerHTML.replace(
+        "Головы щучьи с чесноком да свежайшая сёмгушка.",
+        "Чего сидишь? Порадуй кота, "
+      );
+    }
+
     button.style.display = "inline";
     card_top_text.style.color = "#666666";
     card_top_text.innerHTML = card_top_text.innerHTML.replace(
@@ -63,70 +86,6 @@ function card1() {
   }
 }
 
-function card2() {
-  const card_main = document.getElementsByClassName("container__2")[0];
-  const card = document.getElementsByClassName("card_2")[0];
-  const card_top_text = document.getElementsByClassName("card_text1")[1];
-  const card_bottom = document.getElementsByClassName("text")[1];
-  const card_corner = document.getElementsByClassName("corner")[1];
-  const card_circle = document.getElementsByClassName("card_text5")[1];
-  const button = document.getElementsByClassName("buy")[1];
-
-  function aftermod() {
-    card_top_text.style.color = "#D91667";
-    card_top_text.innerHTML = card_top_text.innerHTML.replace(
-      "Сказочное заморское яство",
-      "Котэ не одобряет?"
-    );
-  }
-  function aftermod_reverse() {
-    card_top_text.style.color = "#666666";
-    card_top_text.innerHTML = card_top_text.innerHTML.replace(
-      "Котэ не одобряет?",
-      "Сказочное заморское яство"
-    );
-  }
-
-  let border = getComputedStyle(card_main);
-  if (
-    border.getPropertyValue("border-color") ==
-      "rgb(0, 0, 0) rgb(22, 152, 217) rgb(22, 152, 217)" ||
-    border.getPropertyValue(
-      "-moz-border-end-color"
-    ) /* Для корректной работы с mozilla */ == "rgb(22, 152, 217)"
-  ) {
-    card_main.style.borderColor = "#D91667";
-    card_main.style.borderTop = "0";
-    card_top_text.style.borderColor = "#D91667";
-    card_corner.style.background =
-      "linear-gradient(-45deg,white, white 28.5px, #D91667 0,#D91667 32px, transparent 0)";
-    card_circle.style.backgroundColor = "#D91667";
-    card_bottom.innerHTML = card_bottom.innerHTML.replace(
-      "Чего сидишь? Порадуй кота, ",
-      "Головы щучьи с чесноком да свежайшая сёмгушка."
-    );
-    button.style.display = "none";
-    card.addEventListener("mouseleave", aftermod);
-  } else {
-    card_main.style.borderColor = "#1698D9";
-    card_main.style.borderTop = "0";
-    card_top_text.style.borderColor = "#1698D9";
-    card_corner.style.background =
-      "linear-gradient(-45deg,white, white 28.5px, #1698D9 0,#1698D9 32px, transparent 0)";
-    card_circle.style.backgroundColor = "#1698D9";
-    card_bottom.innerHTML = card_bottom.innerHTML.replace(
-      "Головы щучьи с чесноком да свежайшая сёмгушка.",
-      "Чего сидишь? Порадуй кота, "
-    );
-    button.style.display = "inline";
-    card_top_text.style.color = "#666666";
-    card_top_text.innerHTML = card_top_text.innerHTML.replace(
-      "Котэ не одобряет?",
-      "Сказочное заморское яство"
-    );
-    card.addEventListener("mouseleave", aftermod_reverse);
-  }
-}
 /* Есть более верный способ сделать некликабельную карту, однако задание требовало
 только одну такую, поэтому пошёл таким путём. */
 function card3() {
